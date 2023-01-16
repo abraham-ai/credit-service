4cb8dbe3a36473cf21736c2e6f9fca0c52d9682d
Jonathan Miller <jon@jmill.dev>
2023-01-16 11:13:55 -0800

    Credit service change

--- src/index.ts
+++ src/index.ts
@@ -10,8 +10,7 @@ const ADMIN_KEY = process.env.ADMIN_KEY;
 const ADMIN_SECRET = process.env.ADMIN_SECRET;
 const EDEN_721DELEGATE_ADDRESS = process.env.EDEN_721DELEGATE_ADDRESS;
 const ALCHEMY_WEBSOCKET_URL = process.env.ALCHEMY_WEBSOCKET_URL;
-const GATEWAY_URL = 'https://gateway-test.abraham.ai';
-
+const GATEWAY_URL = process.env.GATEWAY_URL;
 
 async function main() {
   const address = EDEN_721DELEGATE_ADDRESS as string;
@@ -31,22 +30,29 @@ async function main() {
         caller: caller,
       };
       console.log(info);
-      const credits = creditMap[info.tierId].credits;
-      console.log(`Crediting wallet ${info.beneficiary} with ${credits} credits`);
+      const amount = creditMap[info.tierId].credits;
+      console.log(
+        `Crediting wallet ${info.beneficiary} with ${amount} credits`
+      );
       try {
-        await axios.post(`${GATEWAY_URL}/add_credits`, {
-          adminKey: ADMIN_KEY,
-          adminSecret: ADMIN_SECRET,
-          userType: "ethereum",
-          userId: info.beneficiary,
-          credits: credits
-        });
+        await axios.post(
+          `${GATEWAY_URL}/credits/modify`,
+          {
+            userId: info.beneficiary,
+            amount,
+          },
+          {
+            headers: {
+              "x-api-key": ADMIN_KEY,
+              "x-api-secret": ADMIN_SECRET,
+            },
+          }
+        );
       } catch (error) {
         console.log(error);
       }
     }
   );
 }
-
 
 main();
